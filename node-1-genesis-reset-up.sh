4a848ab931237f473d1295c5cbce8a4b9a647941
Herpiko Dwi Aguno <herpiko@gmail.com>
2018-04-24 20:25:54 +0000

    Update.

--- node-1-genesis-reset-up.sh
+++ node-1-genesis-reset-up.sh
@@ -5,11 +5,12 @@ rm -rf /var/lib/sawtooth/*
 sawtooth keygen --force
 sawset genesis
 sawadm genesis config-genesis.batch
-sawtooth-rest-api -v --connect tcp://192.168.88.9:4004 --bind 192.168.88.9:8008 &
-settings-tp -v --connect tcp://192.168.88.9:4004 &
+sawadm keygen
+sawtooth-rest-api -v --connect tcp://10.2.206.83:4004 --bind 10.2.206.83:8008 &
+settings-tp -v --connect tcp://10.2.206.83:4004 &
 sawtooth-validator -vv \
---endpoint tcp://192.168.88.9:8800 \
---bind component:tcp://192.168.88.9:4004 \
---bind network:tcp://192.168.88.9:8800 \
---peers tcp://192.168.88.10:8800
+--endpoint tcp://10.2.206.83:8800 \
+--bind component:tcp://10.2.206.83:4004 \
+--bind network:tcp://10.2.206.83:8800 \
+--peers tcp://10.3.23.73:8800 \
 --scheduler serial
--- node-2-reset-up.sh
+++ node-2-reset-up.sh
@@ -3,13 +3,12 @@ kill -9 $(ps aux | grep sawtooth | grep -v grep | awk '{print $2}')
 kill -9 $(ps aux | grep settings-tp | grep -v grep | awk '{print $2}')
 rm -rf /var/lib/sawtooth/*
 sawtooth keygen --force
-sawset genesis
-sawadm genesis config-genesis.batch
-sawtooth-rest-api -v --connect tcp://192.168.88.10:4004 --bind 192.168.88.10:8008 &
-settings-tp -v --connect tcp://192.168.88.10:4004 &
+swaadm keygen
+sawtooth-rest-api -v --connect tcp://10.3.23.73:4004 --bind 10.3.23.73:8008 &
+settings-tp -v --connect tcp://10.3.23.73:4004 &
 sawtooth-validator -vv \
---endpoint tcp://192.168.88.10:8800 \
---bind component:tcp://192.168.88.10:4004 \
---bind network:tcp://192.168.88.10:8800 \
---peers tcp://192.168.88.9:8800
+--endpoint tcp://10.3.23.73:8800 \
+--bind component:tcp://10.3.23.73:4004 \
+--bind network:tcp://10.3.23.73:8800 \
+--peers tcp://10.2.206.83:8800 \
 --scheduler serial
