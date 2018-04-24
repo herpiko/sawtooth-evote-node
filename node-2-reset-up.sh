2c5e6d8bd2217505a598858e60627376a1bc991c
Herpiko Dwi Aguno <herpiko@gmail.com>
2018-04-25 02:28:50 +0700

    Update node-2-reset-up.sh

--- node-2-reset-up.sh
+++ node-2-reset-up.sh
@@ -3,8 +3,8 @@ kill -9 $(ps aux | grep sawtooth | grep -v grep | awk '{print $2}')
 kill -9 $(ps aux | grep settings-tp | grep -v grep | awk '{print $2}')
 rm -rf /var/lib/sawtooth/*
 sawtooth keygen --force
- sawset genesis
- sawadm genesis config-genesis.batch
+sawset genesis
+sawadm genesis config-genesis.batch
 sawtooth-rest-api -v --connect tcp://192.168.88.10:4004 --bind 192.168.88.10:8008 &
 settings-tp -v --connect tcp://192.168.88.10:4004 &
 sawtooth-validator -vv \
