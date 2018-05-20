bb9eadc286e12834916f03b90ccbed9d7610c8ef
Herpiko Dwi Aguno <herpiko@gmail.com>
2018-05-21 01:47:45 +0700

    New default port.

--- docker/provinceDPT1/run.sh
+++ docker/provinceDPT1/run.sh
@@ -1,2 +1,4 @@
 cp -vfR ../../transaction-processors tp
-docker rm province-dpt-1; docker build -t province-dpt-1 . && docker run --network national --name province-dpt-1 -ti province-dpt-1
+docker rm province-dpt-1; docker build -t province-dpt-1 .
+rm -rf tp
+docker run -p 3001:3001 -p 4001:4001 --network national --name province-dpt-1 -ti province-dpt-1
