271b1b3393a3fefdc382b4e145136a14c15f365e
Herpiko Dwi Aguno <herpiko@gmail.com>
2018-05-30 11:27:22 +0700

    Three combined distributed bc.

---
+++ docker/province-dpt-91/run.sh
@@ -0,0 +1,8 @@
+#
+# Prepare the network first, docker network create --subnet=172.20.0.0/16 national
+#
+
+cp -vfR ../../transaction-processors tp
+docker rm province-dpt-91; docker build -t province-dpt-91 .
+rm -rf tp
+docker run -p 11391:11391 -p 11291:11291 --network national --ip 172.20.0.91 --name province-dpt-91 -ti province-dpt-91
