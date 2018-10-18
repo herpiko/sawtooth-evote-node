95e49229df53346a08b662dfcb63a6605edec61a
Herpiko Dwi Aguno <herpiko@gmail.com>
2018-10-19 03:54:47 +0700

    Kill first.

--- docker/province-dpt-32/run.sh
+++ docker/province-dpt-32/run.sh
@@ -3,7 +3,7 @@
 #
 
 cp -fR ../../../sawtooth-evote-transaction-processor tp
-docker rm province-dpt-32.skripsi.local; docker build -t province-dpt-32.skripsi.local .
+docker kill province-dpt-32.skripsi.local; docker rm province-dpt-32.skripsi.local; docker build -t province-dpt-32.skripsi.local .
 rm -rf tp
 docker create -p 11332:11332 -p 11232:11232 --network national --ip 172.20.0.32 --name province-dpt-32.skripsi.local -ti province-dpt-32.skripsi.local
 docker start province-dpt-32.skripsi.local
